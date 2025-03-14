4d71fc5c4da10729b6919dd979a6cafadf861e46
Silvegeno <137550428+silvegeno@users.noreply.github.com>
2025-03-14 21:06:25 +0300

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-TOKEN = os.getenv('BOT_TOKEN_LLM')
-api_key = os.getenv("YOUR_OPENAI_API_KEY")  # Устанавливаем ключ OpenAI
-
-# Путь к базе данных
-DB_PATH = "D:\\BD_prog\\education\\Geek_Brains_bot\\books_film_search_bot\\user_books_films.db"
